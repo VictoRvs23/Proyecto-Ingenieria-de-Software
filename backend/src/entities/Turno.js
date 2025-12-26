c2e59fb53089b52b570cc60f7359adf7b8487d3f
Cristobal <cristobal.novoa2301@alumnos.ubiobio.cl>
2025-12-26 20:24:19 -0300

    agrega entidad, rutas y sistema

---
+++ backend/src/entities/Turno.js
@@ -0,0 +1,35 @@
+import { EntitySchema } from "typeorm";
+
+export const Turno = new EntitySchema({
+  name: "Turno",
+  tableName: "turnos",
+  columns: {
+    id: { 
+      primary: true, 
+      type: "int", 
+      generated: "increment" 
+    },
+    date: { 
+      type: "date", 
+      nullable: false 
+    },
+    startTime: { 
+      type: "time", 
+      nullable: false 
+    },
+    endTime: { 
+      type: "time", 
+      nullable: false 
+    },
+    created_at: { 
+      type: "timestamp", 
+      createDate: true, 
+      default: () => "CURRENT_TIMESTAMP" 
+    },
+    updated_at: { 
+      type: "timestamp", 
+      updateDate: true, 
+      default: () => "CURRENT_TIMESTAMP" 
+    },
+  },
+});
